7f184d23c45536ee53c7656f4c46b712c8265aad
Mondragon <fimv705@gmail.com>
2026-03-22 23:01:46 -0600

    Added Euler algorithm

--- euclides.py
+++ euclides.py
@@ -1,6 +1,15 @@
+def euler(n):
+    cont = 0
+    for k in range(1, n):
+        if euclid(n,k)==1:
+            cont += 1
+    return cont
+            
+
 def euclid(a,b):
     if a%b == 0:
         return b
     return euclid(b, a%b)
 
-euclid(18,12)
+print(euclid(18,12))
+print(euler(12))
